7cbf7a396ef78a7520782a6e3f44e20738bb6af6
Krzysztof Siwy <krzysztofsiwy03@gmail.com>
2025-08-06 12:41:39 +0200

    bum

---
+++ Lua/Autorun/init.lua
@@ -0,0 +1,20 @@
+NTCT = {}
+NTCT.Name = "Cybertrauma"
+NTCT.Version = "0.1"
+NTCT.Path = table.pack(...)[1]
+Timer.Wait(function() if NTC ~= nil and NTC.RegisterExpansion ~= nil then NTC.RegisterExpansion(NTCT) end end,1)
+
+if (Game.IsMultiplayer and SERVER) or not Game.IsMultiplayer then
+
+    Timer.Wait(function()
+        if NTC == nil then
+            print("Error loading Cybertrauma: It appears Neurotrauma isn't loaded!")
+            return
+        end
+
+        if NTCyb == nil then
+           print("Error loading Cybertrauma: It appears NT Cybernetics isn't loaded!")
+           return
+        end
+    end, 1)
+end
